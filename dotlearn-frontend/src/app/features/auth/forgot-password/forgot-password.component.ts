fd53f7790f5d50eb078c3e92fb8cd7fbe1811a6c
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-04-17 22:03:31 +0530

    chore: Replace fake forgot-password form with honest coming-soon notice; rebuild prod bundle

--- dotlearn-frontend/src/app/features/auth/forgot-password/forgot-password.component.ts
+++ dotlearn-frontend/src/app/features/auth/forgot-password/forgot-password.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,32 +6,4 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
-  form: FormGroup;
-  isLoading = false;
-  successMessage = '';
-  errorMessage = '';
-
-  constructor(private fb: FormBuilder, private authService: AuthService) {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
-    });
-  }
-
-  onSubmit() {
-    if (this.form.invalid) return;
-    this.isLoading = true;
-
-    this.authService.requestPasswordReset(this.form.value.email).subscribe({
-      next: () => {
-        this.isLoading = false;
-        this.successMessage =
-          'If this email exists, a reset link has been sent. Check your inbox.';
-      },
-      error: () => {
-        this.isLoading = false;
-        this.errorMessage = 'Something went wrong. Please try again.';
-      }
-    });
-  }
-}
+export class ForgotPasswordComponent {}
